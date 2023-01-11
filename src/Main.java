d36a1e8fddfff1441d7dceed9fe211d45fe700d2
Saksham Singh Khanuja <sskhanuja94@gmail.com>
2023-01-12 01:18:42 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        printSquareStar(8);
+    }
+
+    /**
+     * Prints a square star.
+     *
+     * @param number Should be greater than 4.
+     */
+    public static void printSquareStar(int number) {
+        // Checks for invalid value.
+        if (number < 5) {
+            System.out.println("Invalid Value");
+            return;
+        }
+
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < number; j++) {
+                if ((i == 0) || (i == number - 1) || (j == 0) || (j == number - 1) || (j == i) ||
+                        (i == (number - j - 1))) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
